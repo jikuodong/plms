d0f6d2b45a712b88b690328580e919e984f64d43
姬阔东 <jikuodong@froeasy.net>
2019-05-16 16:37:38 +0800

    提交测试SQL数据

---
+++ dataSource/v1.0.0_20190516/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `t_sys_menu` VALUES ('1', '0', '²âÊÔ²Ëµ¥1', 'md-settings', '/person', 'Person', 'person/Person', '', 'person', 1, 1);
+INSERT INTO `t_sys_menu` VALUES ('2', '0', '²âÊÔ²Ëµ¥2', 'ios-body', '/money', 'Money', 'money/Money', NULL, 'money', 1, 1);
+INSERT INTO `t_sys_menu` VALUES ('3', '1', '²Ëµ¥1-1', NULL, 'student', 'Student', 'person/student/Student', NULL, 'person', 1, 1);
+INSERT INTO `t_sys_menu` VALUES ('4', '2', '²Ëµ¥2-2', NULL, 'consumption', 'Consumption', 'money/consumption/Consumption', NULL, 'money', 1, 1);
